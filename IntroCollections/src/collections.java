c662826f5ea711ece8c12b9b572e1e7c6d9bbe1c
alejandromijares <alejandromijares@192.168.0.20>
2016-08-21 10:41:33 -0500

    Iterator with the list1 and list2

--- IntroCollections/src/collections.java
+++ IntroCollections/src/collections.java
@@ -19,8 +19,29 @@ public class collections {
 				for(String y : morethings)
 					list2.add(y);
 		
-		for(int i=0; i<list2.size();i++){
-			System.out.printf("%s ", list2.get(i));
+		for(int i=0; i<list1.size();i++){
+			System.out.printf("%s ", list1.get(i));
+		}	
+		editlist(list1,list2);
+		System.out.println();
+		
+		for(int i=0; i<list1.size();i++){
+			System.out.printf("%s ", list1.get(i));
+		}	
+    }
+	
+	public static void editlist(Collection<String> l1, Collection<String> l2){
+		Iterator<String> it = l1.iterator();
+		while(it.hasNext()){ //Vamos a iterar entre la lista l1 
+			if(l2.contains(it.next())) //Comparamos entre la lista l2 y lista l1 
+				it.remove();//Si la lista l2 tiene algo de la lista l1, lo removemos.
 		}
+	}
 }
-}
+
+
+
+		
+
+
+
