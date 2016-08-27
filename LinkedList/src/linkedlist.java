45cd8492162c74545c2b394a6527713abe1240ce
alejandromijares <alejandromijares@192.168.0.20>
2016-08-26 19:30:49 -0500

    2da parte de LinkedList. metodos como remove, reverse y print

--- LinkedList/src/linkedlist.java
+++ LinkedList/src/linkedlist.java
@@ -18,6 +18,38 @@ public class linkedlist {
 				list2.add(y);
 			list1.addAll(list2);
 			list2 = null;
+			
+			printMe(list1);
+			removeStuff(list1, 2,5);
+			printMe(list1);
+			reverseMe(list1);
 	}
-
+	
+	//printMe Method
+	private static void printMe(List<String> l){
+		for(String b : l){
+			System.out.printf("%s ", b);
+			System.out.println();
+		}
+	}
+	
+	//removeStuff method
+	
+	private static void removeStuff(List<String> l, int from, int to){
+		l.subList(from, to).clear();
+	}
+	
+	//reverseMe
+	
+	private static void reverseMe(List<String> l){
+		ListIterator<String> bobby = l.listIterator(l.size());
+		while(bobby.hasPrevious()){
+			System.out.printf("%s ", bobby.previous());
+		}
+	}
 }
+
+
+
+	
+
